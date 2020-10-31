c314d3eee600a14c4dfb4677c1638ed417649ee8
Gulira91 <d.gulira@gmail.com>
2020-10-31 12:42:56 -0400

    initial commit

--- src/main/java/Halloween.java
+++ src/main/java/Halloween.java
@@ -1,4 +1,9 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
 public class Halloween {
 
-
+    public static void main(String[] args) {
+        System.out.println("From intelliJ");
+    }
 }
--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,5 +2,12 @@ public class HalloweenTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        //are we there yet
+        //lets play a game
+        // its fun to drive so far
+
+        // stay home amd be safe amd wear your mask outside
+
     }
 }
